afe72e675cb2f205760a7d46ca891582be1bf579
maestrojeong <legend4020@snu.ac.kr>
2017-03-20 00:42:43 +0900

    edited

--- save_and_restore/restore.py
+++ save_and_restore/restore.py
@@ -1,17 +1,46 @@
 import tensorflow as tf
 
-v1 = tf.Variable(1.0, name="v3")
-v2 = tf.Variable(2.0, name="v4")
-#v3 = tf.Variable(1.0, name="v5")
-saver = tf.train.Saver()
+def master_initializer():
+    uninitailized_variables=[] 
+    for v in tf.global_variables():
+        try :
+            sess.run(v)
+        except tf.errors.FailedPreconditionError:
+            uninitailized_variables.append(v)
+    return tf.variables_initializer(uninitailized_variables)
 
-with tf.Session() as sess:
-    save_path="./save/model"
-    sess.run(tf.global_variables_initializer())
-    print(sess.run(v1))
-    print(sess.run(v2))
-    saver.restore(sess, save_path)
-    print(sess.run(v1))
-    print(sess.run(v2))
-    print("Model restored.")
+v3 = tf.Variable(1.0, name="v3", trainable=False )
+v4 = tf.Variable(2.0, name="v4")
+save_path="./save/model"
+print("Before restoration")
+print("tf.global_variables()")
+print(tf.global_variables())
+print("tf.trainable_variables()")
+print(tf.trainable_variables())
+print("tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)")
+print(tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES))
+print('tf.get_collection("variables")')
+print(tf.get_collection("variables"))
+print('tf.get_collection("trainable_variable")')
+print(tf.get_collection("trainable_variables"))
 
+# don't have to use
+print("tf.GraphKeys.GLOBAL_VARIABLES")
+print(tf.GraphKeys.GLOBAL_VARIABLES)
+print("tf.GraphKeys.TRAINABLE_VARIABLES")
+print(tf.GraphKeys.TRAINABLE_VARIABLES)
+
+print("After restoration")
+sess = tf.Session()
+saver = tf.train.import_meta_graph('./save/model.meta')
+print("trainable variables")
+print(tf.trainable_variables())
+sess.run(master_initializer())
+saver.restore(sess, save_path)
+#sess.run(tf.global_variables_initializer())
+print("v3 : {}".format(sess.run(v3)))
+print("v4 : {}".format(sess.run(v4)))
+print([v.name for v in tf.trainable_variables()])
+
+for v in tf.global_variables():
+    print(v)
