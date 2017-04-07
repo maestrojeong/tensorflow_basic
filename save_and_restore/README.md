5b2689b032b55fd36c125d48b2e670a8cd25672e
maestrojeong <legend4020@snu.ac.kr>
2017-04-07 15:05:35 +0900

    update

---
+++ save_and_restore/README.md
@@ -0,0 +1,6 @@
+# Environment
+    python = 3.4
+    tensorflow 1.0.1
+
+# Object
+    Basis for save and restore
--- save_and_restore/restore.py
+++ save_and_restore/restore.py
@@ -11,33 +11,27 @@ def master_initializer():
 
 v3 = tf.Variable(1.0, name="v3", trainable=False )
 v4 = tf.Variable(2.0, name="v4")
-save_path="./save/model"
 print("Before restoration")
 print("tf.global_variables()")
 print(tf.global_variables())
 print("tf.trainable_variables()")
 print(tf.trainable_variables())
-print("tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)")
-print(tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES))
 print('tf.get_collection("variables")')
 print(tf.get_collection("variables"))
 print('tf.get_collection("trainable_variable")')
 print(tf.get_collection("trainable_variables"))
 
-# don't have to use
-print("tf.GraphKeys.GLOBAL_VARIABLES")
-print(tf.GraphKeys.GLOBAL_VARIABLES)
-print("tf.GraphKeys.TRAINABLE_VARIABLES")
-print(tf.GraphKeys.TRAINABLE_VARIABLES)
+print("tf.GraphKeys.GLOBAL_VARIABLES : {}".format(tf.GraphKeys.GLOBAL_VARIABLES))
+print("tf.GraphKeys.TRAINABLE_VARIABLES : {}".format(tf.GraphKeys.TRAINABLE_VARIABLES))
 
 print("After restoration")
 sess = tf.Session()
 saver = tf.train.import_meta_graph('./save/model.meta')
 print("trainable variables")
 print(tf.trainable_variables())
+
 sess.run(master_initializer())
-saver.restore(sess, save_path)
-#sess.run(tf.global_variables_initializer())
+saver.restore(sess, "./save/model")
 print("v3 : {}".format(sess.run(v3)))
 print("v4 : {}".format(sess.run(v4)))
 print([v.name for v in tf.trainable_variables()])
